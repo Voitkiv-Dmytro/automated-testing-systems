5531058c1e38072e1674b7870127edeaa684c4a8
Voitkiv-Dmytro <dmytro.voitkiv.pm.2020@lpnu.ua>
2022-09-13 14:27:19 +0300

    add test

---
+++ Calculator/AnalaizerClass.Tests/AnalaizerClassTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace AnalaizerClass.Tests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("System.Data.SqlClient", @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Діма\UnitTestData.mdf;Integrated Security=True;Connect Timeout=30", "UnitTestData", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void EstimateTest()
+        {
+            //Arrange
+            string expression = (string)TestContext.DataRow["expression"];
+            string expected = (string)TestContext.DataRow["expected"];
+            AnalaizerClassLibrary.AnalaizerClass.expression = expression;
+
+            //Act
+            var result = AnalaizerClassLibrary.AnalaizerClass.Estimate();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Calculator/AnalaizerClass.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalaizerClass.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalaizerClass.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51d754c2-23b8-4afc-bab3-59a556953518")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
